56be40f027691903081158072419184ce1aebc35
Alain Marquez <alamar1990@gmail.com>
2021-07-03 15:58:34 -0400

    Set grid layout for main section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,52 @@
 <template>
   <div class='card-container'>
-    <div class='card'> card 1</div>
-    <div class='card'></div>
+    <div class='card'>
+      <div class='finances-container'>
+        <div class='card'>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+        </div>
+        <div class='card'>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+
+        </div>
+      </div>
+      <div class='personal-data-container'>
+
+      </div>
+    </div>
+    <div class='card'>
+      <div class='finances-container'>
+        <div class='card'>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+        </div>
+        <div class='card'>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+        </div>
+        <div class='card'>
+          <h1>TEST</h1>
+          <h1>TEST</h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
