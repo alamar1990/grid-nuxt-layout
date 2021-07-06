fd85968f0647344d1a6b3193ec3e3febe73d7ee5
Alain Marquez <alamar1990@gmail.com>
2021-07-05 20:46:32 -0400

    Set main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class='container'>
-    <div class='row h-100'>
-      <div class='col-sm bg-primary'>
-        <div class='col justify-content-center'>
-          <div class='row h-100'>
-            One of two rows
-          </div>
-          <div class='row h-100'>
-            One of two rows
+  <div class='container-fluid'>
+    <div class='d-sm-flex justify-content-between align-items-center mb-4'>
+      <h3 class='text-dark mb-0'>Dashboard</h3>
+    </div>
+    <div class='row'>
+      <div class='col-md-6 col-xl-3 mb-4'>
+        <div class='card shadow border-left-primary py-2'>
+          <div class='card-body'>
+            <div class='row align-items-center no-gutters'>
+              <div class='col mr-2'>
+                <div class='text-uppercase text-primary font-weight-bold text-xs mb-1'><span>Earnings (monthly)</span>
+                </div>
+                <div class='text-dark font-weight-bold h5 mb-0'><span>$40,000</span></div>
+              </div>
+              <div class='col-auto'><i class='fas fa-calendar fa-2x text-gray-300'></i></div>
+            </div>
           </div>
         </div>
       </div>
-      <div class='col-sm bg-danger'>col-sm-4</div>
+      <div class='col-md-6 col-xl-3 mb-4'>
+        <div class='card shadow border-left-success py-2'>
+          <div class='card-body'>
+            <div class='row align-items-center no-gutters'>
+              <div class='col mr-2'>
+                <div class='text-uppercase text-success font-weight-bold text-xs mb-1'><span>Earnings (annual)</span>
+                </div>
+                <div class='text-dark font-weight-bold h5 mb-0'><span>$215,000</span></div>
+              </div>
+              <div class='col-auto'><i class='fas fa-dollar-sign fa-2x text-gray-300'></i></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class='col-md-6 col-xl-3 mb-4'>
+        <div class='card shadow border-left-info py-2'>
+          <div class='card-body'>
+            <div class='row align-items-center no-gutters'>
+              <div class='col mr-2'>
+                <div class='text-uppercase text-info font-weight-bold text-xs mb-1'><span>Tasks</span></div>
+                <div class='row no-gutters align-items-center'>
+                  <div class='col-auto'>
+                    <div class='text-dark font-weight-bold h5 mb-0 mr-3'><span>50%</span></div>
+                  </div>
+                  <div class='col'>
+                    <div class='progress progress-sm'>
+                      <div class='progress-bar bg-info' aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'
+                           style='width: 50%;'><span class='sr-only'>50%</span></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class='col-auto'><i class='fas fa-clipboard-list fa-2x text-gray-300'></i></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class='col-md-6 col-xl-3 mb-4'>
+        <div class='card shadow border-left-warning py-2'>
+          <div class='card-body'>
+            <div class='row align-items-center no-gutters'>
+              <div class='col mr-2'>
+                <div class='text-uppercase text-warning font-weight-bold text-xs mb-1'><span>Pending Requests</span>
+                </div>
+                <div class='text-dark font-weight-bold h5 mb-0'><span>18</span></div>
+              </div>
+              <div class='col-auto'><i class='fas fa-comments fa-2x text-gray-300'></i></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
   </div>
 
 </template>
